d44a6c5b8877cf3a9ab736d64b61e3eb79d4fc43
ppdxxm <ppdxxm@outlook.com>
2017-06-04 09:17:01 +0800

    use visual studio open

--- test/database.py
+++ test/database.py
@@ -7,7 +7,7 @@ loop = asyncio.get_event_loop()
 
 
 async def test_example():
-    dsn = 'DRIVER={SQLite3 ODBC Driver};Database=sqlite.db'
+    dsn = 'DRIVER={SQLite3 ODBC Driver};Database=../database/sqlite.db'
 
     async with aioodbc.create_pool(dsn=dsn, loop=loop) as pool:
         async with pool.acquire() as conn:
